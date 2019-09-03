d98e67ef9da1b87f00431c8a75c89f3c05000d46
Destiny Tibbitts <dtibbs5145@gmail.com>
2019-09-03 18:02:02 -0400

    I have filled in the server file and I am starting to put down the endpoints in the index file. Currently only have a GET for all blogs.

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const server = require('./server');
+const db = require('./data/db');
+
+server.get('/api/blogs', async (req, res) => {
+    try {
+        const blogs = await db.find();
+        res.status(200).json(blogs);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'There was an error getting the blogs' });
+    }
+});
 
 const port = 5000;
 server.listen(port, () => console.log(`API running on ${port}`));
