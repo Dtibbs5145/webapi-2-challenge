f50be8377e720de19eae714952995d2a8b461f97
Destiny Tibbitts <dtibbs5145@gmail.com>
2019-09-03 18:36:52 -0400

    I have added all the GET endpoints and a POST

--- index.js
+++ index.js
@@ -1,13 +1,55 @@
 const server = require('./server');
 const db = require('./data/db');
 
-server.get('/api/blogs', async (req, res) => {
+server.get('/api/posts', async (req, res) => {
     try {
-        const blogs = await db.find();
-        res.status(200).json(blogs);
+        const post = await db.find();
+        res.status(200).json(post);
     } catch (error) {
         console.log(error);
-        res.status(500).json({ message: 'There was an error getting the blogs' });
+        res.status(500).json({ message: 'There was an error getting the posts' });
+    }
+});
+
+server.get('/api/posts/:id', async (req, res) => {
+    try {
+        const post = await db.findById(req.params.id);
+        if (post) {
+            res.status(200).json(post);
+        } else {
+            res.status(404).json({ message: 'That post could not be found' });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'There was an error getting post' });
+    }
+});
+
+server.get('/api/posts/:id/comments', async (req, res) => {
+    try {
+        const post = await db.findPostComments(req.params.id);
+        if (post) {
+            res.status(200).json(post);
+        } else {
+            res.status(404).json({ message: 'Could not find comments' });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'There was an error getting comment(s)' });
+    }
+});
+
+server.post('/api/posts', async (req, res) => {
+    try {
+        const post = await db.insert(req.body);
+        if (post) {
+            res.status(201).json({ message: 'Posted successfully' });
+        } else {
+            res.status(400).json({ message: 'Could not post' });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'There was an error adding post' });
     }
 });
 
